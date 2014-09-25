d88d609ddb107202b6f27553c02ac45216f97aa7
Kevin Ngo <ngoke@mozilla.com>
2014-09-25 12:54:31 -0700

    redirect after login

--- src/media/js/views/login.js
+++ src/media/js/views/login.js
@@ -1,6 +1,10 @@
 define('views/login', ['l10n', 'user', 'urls', 'z'], function(l10n, user, urls, z) {
     var gettext = l10n.gettext;
 
+    z.page.on('logged_in', function() {
+        z.page.trigger('navigate', [urls.reverse('home')]);
+    });
+
     return function(builder) {
         if (user.logged_in()) {
             z.page.trigger('navigate', [urls.reverse('home')]);
