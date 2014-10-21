a3990d99c691c05ca943cd8d523743472bb83960
Kevin Ngo <ngoke@mozilla.com>
2014-10-21 15:30:12 -0700

    remove trail slash

--- src/media/js/routes.js
+++ src/media/js/routes.js
@@ -4,7 +4,7 @@ var root = '^/curate/';
 
 // Please leave quotes around keys! They're needed for Space Heater.
 var routes = window.routes = [
-    {'pattern': '^/$', 'view_name': 'home'},
+    {'pattern': '^$', 'view_name': 'home'},
     {'pattern': root + 'login$', 'view_name': 'login'},
     {'pattern': root + '403$', 'view_name': '403'},
     {'pattern': root + '$', 'view_name': 'home'},
